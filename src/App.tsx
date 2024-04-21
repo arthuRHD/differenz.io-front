01aa5039e7986be71cc7c73112da5f9626bd5c3b
Arthur RICHARD <arthur.richard@protonmail.com>
2024-04-21 18:47:05 +0200

    :lipstick: adding tailwind to the app

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import logo from './assets/logo.png';
 
 export default function App() {
 
+
   return (
     <body>
-      <h1>differenz.io</h1>
+      <h1 className="text-3xl font-bold underline">differenz.io</h1>
       <span>Upgrade effortlessly</span>
       <img src={logo} alt={"differenzio logo"} />
     </body>
